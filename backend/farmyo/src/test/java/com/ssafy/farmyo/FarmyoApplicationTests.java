b242705d0f7a01e296df3d08dad49f37058d9ecc
권현준 <bonheur9813@naver.com>
2024-03-11 14:50:04 +0900

    🎉 : 백엔드 프로젝트 생성

---
+++ backend/farmyo/src/test/java/com/ssafy/farmyo/FarmyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.farmyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
