41796f5545822c85fe2c99240c17688ff4834e7f
장세정 <wkdtpwjd29@naver.com>
2024-03-12 17:33:55 +0900

    💄 : 하단네브바생성

--- frontend/farmyo/src/router.js
+++ frontend/farmyo/src/router.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login/index"
+import SellerMypage from "./pages/mypage/seller/index"
 
 
 export default function Router (){
   return(
    <Routes>
       <Route path='/' element={<Login />} />
+      <Route path='/mypage/seller' element={<SellerMypage />} />
    </Routes>
   )
 }
