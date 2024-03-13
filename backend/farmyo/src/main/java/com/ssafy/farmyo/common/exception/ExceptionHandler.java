b8627f806a18bd5d09594261b2c0c37a34a5088c
권현준 <bonheur9813@naver.com>
2024-03-13 14:10:12 +0900

    🔥 : 불필요한 코드 삭제
    핸들러 불필요한 로그코드 삭제
    
    Related to : S10P22D209-65

--- backend/farmyo/src/main/java/com/ssafy/farmyo/common/exception/ExceptionHandler.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/common/exception/ExceptionHandler.java
@@ -1,19 +1,13 @@
 package com.ssafy.farmyo.common.exception;
 
 import com.ssafy.farmyo.common.response.BaseResponseBody;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
-@Slf4j
 @RestControllerAdvice
 public class ExceptionHandler {
-
     @org.springframework.web.bind.annotation.ExceptionHandler(CustomException.class)
     public ResponseEntity<? extends BaseResponseBody> exceptions(CustomException e) {
-
-        log.info("핸들러 입장!!");
 
         // 정의한 에러 코드 및 메시지 적용
         BaseResponseBody errorResponse = BaseResponseBody.error(e.getExceptionType().getErrorCode(), e.getMessage());
