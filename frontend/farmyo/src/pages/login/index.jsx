387561900f23ee4d9f7e95df53f299c30e575e44
이정민 <bona3377@naver.com>
2024-03-13 12:47:31 +0900

    💄 : 로그인 화면 추가
    - 아이디, 비밀번호 입력창 생성

--- frontend/farmyo/src/pages/login/index.jsx
+++
@@ -1,7 +0,0 @@
-export default function Login() {
-  return (
-    <div>
-      <p className="font-bold text-3xl">로그인페이지</p>
-    </div>
-  )
-}
---
+++ frontend/farmyo/src/pages/user/login/index.jsx
@@ -0,0 +1,9 @@
+import LoginInput from "../../../component/user/login/index"
+
+export default function LoginMainPage () {
+  return (
+    <div>
+      <LoginInput />
+    </div>
+  )
+}
