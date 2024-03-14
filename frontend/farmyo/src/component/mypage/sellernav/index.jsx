86ddeaf7a54513799508df344b111b9fc55ae7f7
장세정 <wkdtpwjd19@gmail.com>
2024-03-14 16:04:02 +0900

    💄 : 작물등록모달,작물정보보기모달css

--- frontend/farmyo/src/component/mypage/sellernav/index.jsx
+++ frontend/farmyo/src/component/mypage/sellernav/index.jsx
@@ -1,9 +1,15 @@
 import React, { useState,useEffect } from 'react'
 import Me from '../../../image/component/me.png'
 import Next from '../../../image/component/next.png'
+import Sae from '../../../image/component/sae.png'
+import SaeClick from '../../../image/component/saeclick.png'
+import Myfarm from '../myfarm'
+import Mycrops from '../mycrops'
+import ArticleList from '../articlelist'
 
 export default function MypageNavbar() {
   const [selected,setSelected] = useState(null)
+  const [love,setLove] = useState(false)
 
   useEffect(()=>{
     setSelected(0);
@@ -13,6 +19,10 @@ export default function MypageNavbar() {
     setSelected(index)
   }
 
+  const handleLove = () =>{
+    setLove(!love)
+  }
+
   return(
     <div>
       <div style={{height:50,backgroundColor:'#1B5E20'}}>
@@ -20,7 +30,7 @@ export default function MypageNavbar() {
           <h1 className="text-xl font-bold" style={{color:"white"}}>마이페이지</h1>
         </div>
       </div>
-      <div className='flex border-b-2 border-gray-300'style={{height:120}}>
+      <div className='flex border-b-2 border-gray-300' style={{height:140}}>
         <div className='p-5 pt-7'>
           <img src={Me} alt="" />
         </div>
@@ -29,15 +39,24 @@ export default function MypageNavbar() {
           <h4 className='text-sm'>대구광역시 달서구 호산로 126</h4>
           <h4 className='text-sm'style={{color:'gray'}}>상태메시지 추가하기</h4>
         </div>
+        {/* 아래의 디브는 본인의 마이페이지일경우만 보이고 , 즐찾이미지는 구매자일경우만 보임 */}
         <div className='flex justify-center items-center'>
           <img src={Next} alt="" style={{width:50,height:50}}/>
         </div>
+        {/* <div className='flex justify-center items-center p-4' onClick={handleLove}>
+          <img src={ love ? SaeClick : Sae} alt="" style={{width:50,height:50}} />
+        </div> */}
+
       </div>
       <div className='flex justify-around p-3' style={{height:50}}>
         <h1 className='font-bold' style={{ color: selected === 0 ? 'black' : 'gray' }} onClick={()=>{handleClick(0)}}>마이팜</h1>
         <h1 className='font-bold' style={{ color: selected === 1 ? 'black' : 'gray' }} onClick={()=>{handleClick(1)}}>마이작물</h1>
         <h1 className='font-bold' style={{ color: selected === 2 ? 'black' : 'gray' }} onClick={()=>{handleClick(2)}}>게시글</h1>
       </div>
+      {/* 선택된 컴포넌트 조건부 렌더링 */}
+      {selected === 0 && <Myfarm />}
+      {selected === 1 && <Mycrops />}
+      {selected === 2 && <ArticleList />}
     </div>
   )
 }
--- frontend/farmyo/src/pages/mypage/seller/index.jsx
+++ frontend/farmyo/src/pages/mypage/seller/index.jsx
@@ -1,11 +1,9 @@
 import MypageNavbar from "../../../component/mypage/sellernav"
-import MyFarm from "../../../component/mypage/myfarm"
 
 export default function SellerMypage () {
   return (
     <div>
       <MypageNavbar />
-      <MyFarm />
     </div>
   )
 }
