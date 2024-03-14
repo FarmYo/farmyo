dea271104edf9bd8d9e314951703a4e857b62762
조현제 <abb6575@naver.com>
2024-03-14 10:32:53 +0900

    ✨ : 엔티티추가
    각 엔티티 추가

--- backend/farmyo/src/main/java/com/ssafy/farmyo/common/entity/BaseTime.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/common/entity/BaseTime.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
