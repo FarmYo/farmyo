6470cf74b42e444d32219d968af78de034550090
김준엽 <wnsduq1025@gmail.com>
2024-03-19 17:58:17 +0900

    🚧 : createTrade 작업 도중 저장

--- backend/farmyo/src/main/java/com/ssafy/farmyo/trade/service/TradeService.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/trade/service/TradeService.java
@@ -1,5 +1,7 @@
 package com.ssafy.farmyo.trade.service;
 
+import com.ssafy.farmyo.entity.Board;
+import com.ssafy.farmyo.entity.Chat;
 import com.ssafy.farmyo.entity.User;
 import com.ssafy.farmyo.trade.dto.TradeDto;
 
@@ -7,8 +9,8 @@ import java.util.List;
 
 public interface TradeService {
 
-    int createTrade(User user, TradeDto tradeDto);
-    List<TradeDto> getListTrade(int id);
+    int createTrade(int sellerId, int buyerId, int boardId, int chatId, TradeDto tradeDto);
+    List<TradeDto> getTradeList(int userId);
     TradeDto getTrade(int id);
     void updateTrade(TradeDto tradeDto);
 
