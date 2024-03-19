5a25c8c11ced9a5582ae866aa6541d02c6704188
권현준 <bonheur9813@naver.com>
2024-03-19 11:01:55 +0900

    ✨ : 유저 회원가입 기능 구현
    - 유저 회원가입 기능구현
      성공시 201 & 사용자 식별 번호 반환

---
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/user/dto/JoinReqDto.java
@@ -0,0 +1,57 @@
+package com.ssafy.farmyo.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class JoinReqDto {
+
+    // 아이디
+    @NotBlank(message = "유효하지 않은 아이디값입니다.")
+    private String loginId;
+
+    // 비밀번호
+    @NotBlank
+    private String password;
+
+    // 닉네임
+    @NotBlank
+    private String nickname;
+
+    // 전화번호
+    @NotBlank
+    private String telephone;
+
+    // 이메일
+    @Email
+    @NotBlank
+    private String email;
+
+
+    // 예금주
+    @NotBlank
+    private String depositor;
+
+    // 은행명
+    @NotBlank
+    private String bank;
+
+    // 계좌번호
+    @NotBlank
+    private String account;
+
+
+    // 우편번호
+    @NotBlank
+    private String addressCode;
+
+    // 법정주소
+    @NotBlank
+    private String addressLegal;
+
+    // 상세주소
+    @NotBlank
+    private String addressDetail;
+
+}
--- backend/farmyo/src/main/java/com/ssafy/farmyo/user/repository/UserRepository.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/user/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.ssafy.farmyo.user.repository;
 
-public interface UserRepository {
+import com.ssafy.farmyo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
