2e22dfb88e5c02dd0211c84d7f617454a7a34b6d
조현제 <abb6575@naver.com>
2024-03-20 22:10:25 +0900

    🚧 : pull develop

--- backend/farmyo/src/main/java/com/ssafy/farmyo/crop/controller/CropController.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/crop/controller/CropController.java
@@ -2,6 +2,7 @@ package com.ssafy.farmyo.crop.controller;
 
 import com.ssafy.farmyo.common.response.BaseResponseBody;
 import com.ssafy.farmyo.crop.dto.AddCropReqDto;
+import com.ssafy.farmyo.crop.dto.UpdateImgReqDto;
 import com.ssafy.farmyo.crop.dto.FindCropCategoryDto;
 import com.ssafy.farmyo.crop.service.CropService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -39,6 +40,19 @@ public class CropController {
         return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponseBody.of(0, savedCropId));
 
     }
+
+
+    //대표사진수정  사진 넣는걸로 바꿔야한다 url이 아니라 수정해야함 임시용
+    @Operation(summary = "대표사진수정 아직 미완성", description = "/crops\n\n 작물의 대표 사진을 변경한다.")
+    @PatchMapping("/{cropId}")
+    @ApiResponse(responseCode = "204", description = "성공 \n\n Success 반환")
+    public ResponseEntity<? extends BaseResponseBody> updateCropImg(@PathVariable Integer cropId, @RequestBody UpdateImgReqDto updateImgReqDto) {
+        cropService.updateCropImgUrl(cropId, updateImgReqDto.getCropImgUrl());
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0,"성공적으로 변경되었습니다."));
+    }
+
+
+
 
 
 
--- backend/farmyo/src/main/java/com/ssafy/farmyo/crop/service/CropServiceImpl.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/crop/service/CropServiceImpl.java
@@ -9,12 +9,15 @@ import com.ssafy.farmyo.crop.repository.CropCategoryRepository;
 import com.ssafy.farmyo.crop.repository.CropRepository;
 import com.ssafy.farmyo.entity.Crop;
 import com.ssafy.farmyo.entity.CropCategory;
+import com.ssafy.farmyo.entity.Farm;
 import com.ssafy.farmyo.entity.Farmer;
+import com.ssafy.farmyo.user.repository.FarmerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,23 +25,29 @@ import java.util.stream.Collectors;
 public class CropServiceImpl implements CropService {
     private final CropRepository cropRepository;
     private final CropCategoryRepository cropCategoryRepository;
+    private final FarmerRepository farmerRepository;
     private final CropContractService cropContractService;
 
 
 
+    //작물 등록
     @Override
     public Integer addCrop(AddCropReqDto addCropReqDto) {
 
         //농부 조회(아직 모름
-        Farmer farmer;
+        Optional<Farmer> optionalFarmer = farmerRepository.findByUserId(addCropReqDto.getFarmer_id());
+        if (optionalFarmer.isEmpty()) {
+            throw new CustomException(ExceptionType.USER_NOT_EXIST);
+        }
+        Farmer farmer = optionalFarmer.get();
 
         //작물 카테고리 조회
         CropCategory cropCategory = cropCategoryRepository.findById(addCropReqDto.getCropCategoryId())
-                .orElseThrow(() -> new CustomException(ExceptionType.CROP_NOT_EXIST));
+                .orElseThrow(() -> new CustomException(ExceptionType.CATEGORY_NOT_EXIST));
 
         //농부는 일단 어떻게 받아서 넣을지 몰라서null처리
         Crop crop = Crop.builder()
-                .farmer(null)
+                .farmer(farmer)
                 .cropCategory(cropCategory)
                 .cropName(addCropReqDto.getCropName())
                 .cropBlockchainAddress(null)
@@ -54,6 +63,30 @@ public class CropServiceImpl implements CropService {
         return crop.getId();
     }
 
+
+    //작물 사진 업데이트
+    @Override
+    public void updateCropImgUrl(Integer cropId, String cropImgUrl) {
+        Optional<Crop> cropOptional = cropRepository.findById(cropId);
+        if (cropOptional.isPresent()) {
+            Crop crop = cropOptional.get();
+            crop.updateCropImgUrl(cropImgUrl);
+            cropRepository.save(crop);
+        } else {
+            throw new CustomException(ExceptionType.CROP_NOT_EXIST);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+    //모든 카테고리 조회
     @Override
     public List<FindCropCategoryDto> findAllCropCategories() {
         return cropCategoryRepository.findAll().stream()
