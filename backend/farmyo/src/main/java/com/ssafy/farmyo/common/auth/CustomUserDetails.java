1e73796129cf638e576ca192a34a886ebcaa1662
권현준 <bonheur9813@naver.com>
2024-03-20 22:45:11 +0900

    ✨ : 로그인 기능 구현
    - 로그인 Dto 작성
    - DB에 유저 엔티티를 가져오는 CustomUserDetailsService 작성
    - 유저 정보 넣어놓는 CustomUserDetails 작성
    - ID로 엔티티 가져오는 메서드 작성
    - 로그인 로직 처리 필터 작성
    
    Related to : S10P22D209-97

---
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/common/auth/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.ssafy.farmyo.common.auth;
+
+import com.ssafy.farmyo.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // 식별 ID
+    public int getId() {return user.getId();}
+
+    // 유저 직업
+    public int getJob() {return user.getJob();}
+}
