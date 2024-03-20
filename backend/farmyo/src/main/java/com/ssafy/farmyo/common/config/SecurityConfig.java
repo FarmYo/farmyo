d9a5da84feaa87c7e8b7ba63fe02ffb2b4d91e99
조현제 <abb6575@naver.com>
2024-03-20 17:26:24 +0900

    🚧 : 작업 도중 저장
    아직 작업중입니다.

--- backend/farmyo/src/main/java/com/ssafy/farmyo/common/config/SecurityConfig.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/common/config/SecurityConfig.java
@@ -1,72 +1,72 @@
-package com.ssafy.farmyo.common.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-
-import java.util.Arrays;
-import java.util.List;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    private static final String[] PERMIT_URL_ARRAY = {
-            /* api */
-            "/api/**",
-            /* swagger v3 */
-            "/v3/api-docs/**",
-            "/swagger-ui/**",
-            /* image */
-            "/image/**"
-    };
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        // 불필요한 특성 비활성화
-        http.csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable);
-
-        // 세션 설정
-        http.sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        // 경로별 인가 작업
-        http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("*").permitAll());
-//                .requestMatchers(PERMIT_URL_ARRAY).permitAll()
-//                .anyRequest().authenticated());
-
-        // 필터 등록
-
-        // CORS 설정
-        http.cors(cors -> cors.configurationSource(request -> {
-            CorsConfiguration corsConfig = new CorsConfiguration();
-            corsConfig.setAllowedOriginPatterns(List.of("*"));
-            corsConfig.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-            corsConfig.setAllowedHeaders(List.of("*"));
-            corsConfig.addExposedHeader("Authorization");
-            corsConfig.setAllowCredentials(true);
-
-            return corsConfig;
-        }));
-
-        return http.build();
-    }
-
-}
+//package com.ssafy.farmyo.common.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    private static final String[] PERMIT_URL_ARRAY = {
+//            /* api */
+//            "/api/**",
+//            /* swagger v3 */
+//            "/v3/api-docs/**",
+//            "/swagger-ui/**",
+//            /* image */
+//            "/image/**"
+//    };
+//
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//        // 불필요한 특성 비활성화
+//        http.csrf(AbstractHttpConfigurer::disable)
+//                .formLogin(AbstractHttpConfigurer::disable)
+//                .httpBasic(AbstractHttpConfigurer::disable);
+//
+//        // 세션 설정
+//        http.sessionManagement(sessionManagement ->
+//                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+//        // 경로별 인가 작업
+//        http.authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("*").permitAll());
+////                .requestMatchers(PERMIT_URL_ARRAY).permitAll()
+////                .anyRequest().authenticated());
+//
+//        // 필터 등록
+//
+//        // CORS 설정
+//        http.cors(cors -> cors.configurationSource(request -> {
+//            CorsConfiguration corsConfig = new CorsConfiguration();
+//            corsConfig.setAllowedOriginPatterns(List.of("*"));
+//            corsConfig.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+//            corsConfig.setAllowedHeaders(List.of("*"));
+//            corsConfig.addExposedHeader("Authorization");
+//            corsConfig.setAllowCredentials(true);
+//
+//            return corsConfig;
+//        }));
+//
+//        return http.build();
+//    }
+//
+//}
---
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/crop/dto/AddCropReqDto.java
@@ -0,0 +1,37 @@
+package com.ssafy.farmyo.crop.dto;
+
+import com.ssafy.farmyo.entity.Crop;
+import com.ssafy.farmyo.entity.CropCategory;
+import com.ssafy.farmyo.entity.Farmer;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AddCropReqDto {
+
+    //유저는 어떻게 받을지 모르겠음
+    private Farmer farmer;
+
+    @NotEmpty(message = "카테고리 골라주세요")
+    private Integer cropCategoryId;
+
+    @NotBlank(message = "작물이름 적어주세요")
+    private String cropName;
+
+    private String cropBlockchainAddress;
+    private String cropImgUrl;
+
+    @Builder
+    public AddCropReqDto(Farmer farmer, Integer cropCategoryId, String cropName, String cropBlockchainAddress, String cropImgUrl) {
+        this.farmer = farmer;
+        this.cropCategoryId = cropCategoryId;
+        this.cropName = cropName;
+        this.cropBlockchainAddress = cropBlockchainAddress;
+        this.cropImgUrl = cropImgUrl;
+    }
+
+}
