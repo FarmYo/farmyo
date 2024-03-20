c65d5573a40ebca9591957fbe99113eb900dadea
권현준 <bonheur9813@naver.com>
2024-03-20 22:45:11 +0900

    ✨ : 로그인 기능 구현
    - 로그인 Dto 작성
    - DB에 유저 엔티티를 가져오는 CustomUserDetailsService 작성
    - 유저 정보 넣어놓는 CustomUserDetails 작성
    - ID로 엔티티 가져오는 메서드 작성
    - 로그인 로직 처리 필터 작성
    
    Related to : S10P22D209-97

--- backend/farmyo/src/main/java/com/ssafy/farmyo/user/repository/UserRepository.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/user/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.ssafy.farmyo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    // 로그인 아이디로 회원 조회
+    Optional<User> findByLoginId(String loginId);
 }
