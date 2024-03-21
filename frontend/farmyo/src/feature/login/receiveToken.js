1948989f2ffd530c53c374908abc50ff08b9bab5
이정민 <bona3377@naver.com>
2024-03-21 11:26:04 +0900

    ✨ :  로그인 기능 추가
    - 로그인 기능 구현

--- frontend/farmyo/src/feature/login/receiveToken.js
+++ frontend/farmyo/src/feature/login/receiveToken.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { cookies } from "utils/cookies";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setAccessToken } from "accessSlice";
@@ -7,30 +8,43 @@ import { logout } from "authSlice";
 export function useTokenCheck() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const accessToken = useSelector((store) => store.access.accessToken);
+  // const accessToken = useSelector((store) => store.access.accessToken);
+  const accessToken = useSelector((store) => store.access);
   
   // accessToken 상태 판단
   const checkAccess = async (accessToken) => {
-    const headers = { Authorization: `Bearer ${accessToken}` };
+    const headers = { Authorization: {accessToken} };
     // 모든 페이지에서 토큰 상태 확인
     return axios.get(window.location.href, { headers });
   };
 
     // cookie에서 token 값 가지고 오기
-    const getCookie = (token) => {
-      const cookies = document.cookie.split(";");
-      for (const cookie of cookies) {
-        const parts = cookie.split("=");
-        if (parts[0].trim() === token) {
-          return parts[1].trim();
-        }
-      }
-      return null;
-    };
+    // const getCookie = (token) => {
+    //   const cookies = document.cookie.split(";");
+    //   for (const cookie of cookies) {
+    //     const parts = cookie.split("=");
+    //     if (parts[0].trim() === token) {
+    //       return parts[1].trim();
+    //     }
+    //   }
+    //   return null;
+    // };
+    // const getCookie = (token) => {
+      // const cookies = document.cookie.refresh;
+      // for (const cookie of cookies) {
+      //   const parts = cookie.split("=");
+      //   if (parts[0].trim() === token) {
+      //     return parts[1].trim();
+      //   }
+      // }
+      // return null;
+    // };
   
   // refreshToken 기반으로 새로운 accessToken 발급
   const refreshAccessToken = async () => {
-    const refreshToken = getCookie("refreshToken");
+    // const refreshToken = getCookie("refresh");
+    // const refreshToken = document.cookie.refresh;
+    const refreshToken = cookies.get("refresh");
     const data = { refreshToken };
     // 모든 페이지에서 토큰 유효성 확인
     return axios.post(window.location.href, data, { withCredentials: true });
@@ -40,13 +54,13 @@ export function useTokenCheck() {
     try {
       // accessToken 유효성 검사 결과
       const response = await checkAccess(accessToken);
-      if (response.dataHeader.successCode === 0) {
+      if (response.data.status === 200) {
         return true;
         // accessToken 만료
-      } else if (response.dataHeader.successCode === 1) {
+      } else if (response.data.status === 401) {
         const refreshResponse = await refreshAccessToken();
         // refreshToken 살아있어서 accessToken 갱신
-        if (refreshResponse.dataHeader.successCode === 0) {
+        if (response.data.status === 200) {
           // redux store에 새로운 accessToken 업데이트
           dispatch(setAccessToken(refreshResponse.headers.authorization));
           return true;
