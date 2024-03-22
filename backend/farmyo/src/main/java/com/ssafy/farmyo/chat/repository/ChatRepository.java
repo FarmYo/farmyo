38fc8b005b4efe7d53c877d93ef571076e40bd0d
김준엽 <wnsduq1025@gmail.com>
2024-03-22 09:00:08 +0900

    🔨: trade 코드 수정

--- backend/farmyo/src/main/java/com/ssafy/farmyo/chat/repository/ChatRepository.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/chat/repository/ChatRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatRepository extends JpaRepository<Chat, Long> {
-
-    Chat findById(int id);
+public interface ChatRepository extends JpaRepository<Chat, Integer> {
 
 }
