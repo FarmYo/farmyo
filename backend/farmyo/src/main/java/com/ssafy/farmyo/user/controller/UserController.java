cf7eddb1303a10852b35960d4891cc608a538150
권현준 <bonheur9813@naver.com>
2024-03-24 02:52:52 +0900

    ✨ : 이메일 인증코드 확인 기능 구현
    - 해당 이메일의 인증코드 확인 기능
      이메일이 잘못 입력되거나 제한시간을 넘겨 없어진다면 -> U-004
      인증코드가 다르다면 -> U005

--- backend/farmyo/src/main/java/com/ssafy/farmyo/user/controller/UserController.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.ssafy.farmyo.common.auth.CustomUserDetails;
 import com.ssafy.farmyo.common.response.BaseResponseBody;
 import com.ssafy.farmyo.user.dto.JoinReqDto;
 import com.ssafy.farmyo.user.dto.VerifyEmailReqDto;
+import com.ssafy.farmyo.user.dto.VerifyCodeReqDto;
 import com.ssafy.farmyo.user.service.MailService;
 import com.ssafy.farmyo.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -62,6 +63,17 @@ public class UserController {
 
         // 메일 전송 후 코드 받기
         mailService.sendJoinMessage(verifyEmailReqDto.getEmail());
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, "Success"));
+    }
+
+    @Operation(summary = "이메일 인증 확인", description = "/users/auth/check\n\n 사용자는 이메일 인증 확인을 한다.")
+    @PostMapping("/auth/check")
+    @ApiResponse(responseCode = "200", description = "성공 \n\n Success 반환")
+    public ResponseEntity<? extends BaseResponseBody> verifyEmailCode(@Valid @RequestBody VerifyCodeReqDto verifyCodeReqDto) {
+
+        // 인증 코드 확인 후 토큰 반환 (주석 이유 : 추가 로직 필요!)
+        // String token = mailService.validateAuthCode(verifyCodeReqDto);
+        mailService.validateAuthCode(verifyCodeReqDto);
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, "Success"));
     }
 
