cd15b4f946dfba6e4ae6635f091a7c9b2325fed3
권현준 <bonheur9813@naver.com>
2024-03-25 16:08:22 +0900

    ✨ : 회원 정보 조회 기능 구현
    성공시 상태코드 200 및 정보 DTO 반환
    
    Related to : S10P22D209-276

--- backend/farmyo/src/main/java/com/ssafy/farmyo/user/repository/UserRepository.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/user/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.ssafy.farmyo.user.repository;
 
 import com.ssafy.farmyo.entity.User;
+import com.ssafy.farmyo.user.dto.UserResDto;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,4 +16,11 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     // 이메일로 회원 조회
     Optional<User> findByEmail(String email);
+
+    // 회원 정보 조회
+    @Query("SELECT new com.ssafy.farmyo.user.dto.UserResDto(u.loginId, u.email, u.nickname, u.telephone, u.comment, u.account, a.addressCode, a.addressDetail, a.addressLegal) " +
+            "FROM User u join Address a " +
+            "ON u.id = a.user.id " +
+            "WHERE u.id = :id ")
+    UserResDto getUserInfoById(@Param("id") Integer id);
 }
