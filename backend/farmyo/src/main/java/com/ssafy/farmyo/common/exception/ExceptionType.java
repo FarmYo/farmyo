0d097046d53ced00a671e8a1e3728f621ffc04a1
조현제 <abb6575@naver.com>
2024-03-26 14:57:03 +0900

    ✨ : 게시글 업데이트(patch)기능 추가
    성공시 200
    작성자와 접속한 유저가 다를 시 BOARD_NOT_EXIST 예외

--- backend/farmyo/src/main/java/com/ssafy/farmyo/common/exception/ExceptionType.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/common/exception/ExceptionType.java
@@ -41,16 +41,19 @@ public enum ExceptionType {
     ACCOUNT_NOT_EXIST(HttpStatus.UNAUTHORIZED, "A-001", "해당 유저의 계좌가 존재하지 않습니다."),
 
     // 게시판
-    BOARD_NOT_EXIST(HttpStatus.BAD_REQUEST, "B-001", "존재하지 않는 게시판입니다."),
+    BOARD_NOT_EXIST(HttpStatus.NOT_FOUND, "B-001", "존재하지 않는 게시판입니다."),
     CROP_NOT_ASSOCIATED_WITH_BOARD(HttpStatus.BAD_REQUEST, "B-002", "게시판에 연결된 작물이 존재하지 않습니다."),
     CROPCATEGORY_NOT_ASSOCIATED_WITH_BOARD(HttpStatus.BAD_REQUEST, "B-003", "판매게시판에 연결된 작물카테고리가 존재하지 않습니다."),
-    FARMER_CANNOT_POST_BUY_BOARD(HttpStatus.BAD_REQUEST, "B-004", "농부는 구매 게시판을 작성할 수 없습니다."),
+    FARMER_CANNOT_POST_BUY_BOARD(HttpStatus.FORBIDDEN, "B-004", "농부는 구매 게시판을 작성할 수 없습니다."),
     QUANTITY_INVALID(HttpStatus.BAD_REQUEST, "B-005", "게시판의 수량이 유효하지 않습니다."),
     PRICE_INVALID(HttpStatus.BAD_REQUEST, "B-006", "게시판의 가격이 유효하지 않습니다."),
     CROP_NOT_OWNED_BY_FARMER(HttpStatus.BAD_REQUEST, "B-007", "작물이 로그인한 농부의 소유가 아닙니다."),
     TITLE_NOT_EXIST(HttpStatus.BAD_REQUEST, "B-008", "글 제목이 없습니다."),
     CONTENT_NOT_EXIST(HttpStatus.BAD_REQUEST, "B-009", "글 내용이 없습니다."),
     BOARDTYPE_INVALID(HttpStatus.BAD_REQUEST, "B-010", "보드타입이 잘못되었습니다."),
+    USER_NOT_AUTHOR(HttpStatus.FORBIDDEN, "B-011", "작성자만 수정 또는 삭제할 수 있습니다."),
+
+
 
     // 채팅
     CHAT_NOT_EXIST(HttpStatus.BAD_REQUEST, "M-001", "존재하지 않는 채팅방입니다.");
