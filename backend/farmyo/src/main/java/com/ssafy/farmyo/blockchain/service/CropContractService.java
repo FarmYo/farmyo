add84f15605dcd6a49e7d282767226125d3047a5
조현제 <abb6575@naver.com>
2024-03-26 22:21:49 +0900

    ✨ : 새작물등록 블록체인 기능 추가
    실패하면 BLOCKCHAIN FAILED TO CREATE 에러

--- backend/farmyo/src/main/java/com/ssafy/farmyo/blockchain/service/CropContractService.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/blockchain/service/CropContractService.java
@@ -6,10 +6,17 @@ import com.ssafy.farmyo.common.exception.ExceptionType;
 import org.springframework.stereotype.Service;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.EthGasPrice;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.RawTransactionManager;
+import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.ContractGasProvider;
 import org.web3j.tx.gas.StaticGasProvider;
+import org.web3j.tx.response.PollingTransactionReceiptProcessor;
+import org.web3j.tx.response.TransactionReceiptProcessor;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.List;
 
@@ -17,58 +24,80 @@ import java.util.List;
 @Service
 public class CropContractService {
 
-    private final CropContract cropContract;
-    private final Web3j web3j;
+    private CropContract cropContract;
+    private Web3j web3j;
 
-    public CropContractService() {
+    private Credentials credentials;
+
+    private TransactionManager transactionManager;
+
+    public CropContractService() throws IOException {
 
         String rpcUrl = "https://rpc2.sepolia.org";
-        String privateKey = "a9e335a9f555e22b10d827b09583725038c3b48e72bc4530289cd72090f4711c";
-        String contractAddress = "0x06BB5EF9f30263d2537f545E28Dfd12f90ad57Ea";
+        String privateKey = "59f616cf0e5003164c921f8172c9da1e7b5c1e1556eca22c0f0c9ef94a2a5b42";
+        String contractAddress = "0x89e67b1cd96337bd5eaa87e83cb8a9917320411a";
+        long chainId = 11155111;
 
 
         this.web3j = Web3j.build(new HttpService(rpcUrl)); // url과 web3j을 통해 해당 코인네트워크 접속
-        Credentials credentials = Credentials.create(privateKey); // 개인 키
+        this.credentials = Credentials.create(privateKey);// 개인 키
 
-        BigInteger gasPrice = BigInteger.valueOf(20_000_000_000L);
-        BigInteger gasLimit = BigInteger.valueOf(4_300_000);
+        EthGasPrice ethGasPrice = web3j.ethGasPrice().send();
+        BigInteger gasPrice = ethGasPrice.getGasPrice();
 
-        ContractGasProvider gasProvider = new StaticGasProvider(gasPrice, gasLimit);
+        TransactionReceiptProcessor receiptProcessor = new PollingTransactionReceiptProcessor(
+                web3j,
+                TransactionManager.DEFAULT_POLLING_FREQUENCY,
+                TransactionManager.DEFAULT_POLLING_ATTEMPTS_PER_TX_HASH
+        );
 
-        this.cropContract = CropContract.load(contractAddress, web3j, credentials, gasProvider);
+        this.transactionManager = new RawTransactionManager(
+                web3j,
+                credentials,
+                chainId,
+                receiptProcessor
+        );
+
+        // CropData 스마트 계약 로드, ContractGasProvider를 사용하여 가스 가격 및 한도 설정
+        this.cropContract = CropContract.load(
+                contractAddress,
+                web3j,
+                transactionManager,
+                new ContractGasProvider() {
+                    @Override
+                    public BigInteger getGasPrice(String contractFunc) {
+                        return gasPrice; // 동적으로 조회된 가스 가격 사용
+                    }
+
+                    @Override
+                    public BigInteger getGasPrice() {
+                        return gasPrice; // 동적으로 조회된 가스 가격 사용
+                    }
+
+                    @Override
+                    public BigInteger getGasLimit(String contractFunc) {
+                        return BigInteger.valueOf(3000000); // 예제 가스 한도
+                    }
+
+                    @Override
+                    public BigInteger getGasLimit() {
+                        return BigInteger.valueOf(3000000); // 예제 가스 한도
+                    }
+                }
+        );
     }
 
-    public void addBasicInfo(BigInteger cropPK, String cropName, String land, BigInteger createdAt) {
-        try {
-            cropContract.addBasicInfo(cropPK, cropName, land, createdAt).send();
-        } catch (Exception e) {
-            System.out.println("왜ㅔ실패지");
-            throw new CustomException(ExceptionType.BLOCKCHAIN_FAILED_TO_CREATE);
-        }
 
+    public TransactionReceipt addBasicInfo(BigInteger cropPK, String cropName, String land, BigInteger createdAt) throws Exception {
+        return cropContract.addBasicInfo(cropPK, cropName, land, createdAt).send();
     }
 
-    public void addUsageInfo(BigInteger cropPK, String drugName, String drugKind, BigInteger createdAt)  {
-        try {
-            cropContract.addUsageInfo(cropPK, drugName, drugKind, createdAt).send();
-        } catch (Exception e) {
-            throw new CustomException(ExceptionType.BLOCKCHAIN_FAILED_TO_CREATE);
-        }
+    public TransactionReceipt addUsageInfo(BigInteger cropPK, String drugName, String drugKind, BigInteger createdAt) throws Exception {
+        return cropContract.addUsageInfo(cropPK, drugName, drugKind, createdAt).send();
     }
 
-    public void addContestInfo(BigInteger cropPK, String contestName, String win, BigInteger createdAt) throws Exception {
-        cropContract.addContestInfo(cropPK, contestName, win, createdAt).send();
-    }
-
-    public CropContract.BasicInfo getBasicInfo(BigInteger cropPK) throws Exception {
-        return cropContract.getBasicInfo(cropPK).send();
-    }
-
-    public List<CropContract.UsageInfo> getUsageInfos(BigInteger cropPK) throws Exception {
-        return cropContract.getUsageInfos(cropPK).send();
-    }
-
-    public List<CropContract.ContestInfo> getContestInfos(BigInteger cropPK) throws Exception {
-        return cropContract.getContestInfos(cropPK).send();
+    public TransactionReceipt addContestInfo(BigInteger cropPK, String contestName, String win, BigInteger createdAt) throws Exception {
+        return cropContract.addContestInfo(cropPK, contestName, win, createdAt).send();
     }
 }
+
