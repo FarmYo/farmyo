09a7714d5c9dc39bae783aca0cdd431068d818cd
권현준 <bonheur9813@naver.com>
2024-03-29 03:39:34 +0900

    🔨 : 회원 정보 조회 시 프로필 추가
    - 응답 DTO에 프로필 필드 추가 및 필드 전체 주석 추가
    - JPQL 쿼리 필드 추가

--- backend/farmyo/src/main/java/com/ssafy/farmyo/user/dto/UserResDto.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/user/dto/UserResDto.java
@@ -10,23 +10,35 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserResDto {
 
+    // 로그인 아이디
     public String loginId;
 
+    // 이메일
     public String email;
 
+    // 닉네임
     public String nickname;
 
+    // 전화번호
     public String telephone;
 
+    // 한 줄 소감
     public String comment;
 
+    // 계좌
     public Account account;
 
+    // 주소 코드
     public String addressCode;
 
+    // 법정 코드
     public String addressLegal;
 
+    // 상세 주소
     public String addressDetail;
+
+    // 프로필 이미지
+    public String profile;
 }
 
 
