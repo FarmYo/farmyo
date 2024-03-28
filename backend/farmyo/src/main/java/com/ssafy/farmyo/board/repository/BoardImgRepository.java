972a840a702e84546256eea846888a63c1bece32
조현제 <abb6575@naver.com>
2024-03-28 21:57:31 +0900

    ✨ : 새팜요 게시판 사진  수정 기능 추가

--- backend/farmyo/src/main/java/com/ssafy/farmyo/board/repository/BoardImgRepository.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/board/repository/BoardImgRepository.java
@@ -3,6 +3,9 @@ package com.ssafy.farmyo.board.repository;
 import com.ssafy.farmyo.entity.BoardImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardImgRepository extends JpaRepository<BoardImg, Integer> {
 
+    List<BoardImg> findByBoardId(Integer boardId);
 }
