63aff3f1096d25a87a0745e329dce911546e77d6
이정민 <bona3377@naver.com>
2024-03-28 09:33:14 +0900

    🔨 : 마이페이지 문구 수정

--- frontend/farmyo/src/component/mypage/sellernav/index.jsx
+++ frontend/farmyo/src/component/mypage/sellernav/index.jsx
@@ -11,12 +11,14 @@ import ArticleList from '../articlelist'
 
 
 export default function MypageNavbar() {
+  const [userInfo, setUserInfo] = useState([])
   const [selected,setSelected] = useState(null)
   const [love,setLove] = useState(false)
   const navigate = useNavigate()
 
   useEffect(()=>{
     setSelected(0);
+    getUserInfo();
   },[])
 
   const handleClick = (index) => {
@@ -27,11 +29,23 @@ export default function MypageNavbar() {
     setLove(!love)
   }
 
+  const getUserInfo = (() => {
+    api.get('users')
+    .then((res) => {
+      console.log('정보 받아오기 성공', res.data, res.data.dataBody)
+      setUserInfo(res.data.dataBody)
+    })
+    .catch((err) => {
+      console.log('정보 받아오기 실패', err)
+    })
+  })
+
   const logOut = (() => {
     api.post('users/logout')
     .then((res) => {
       console.log('로그아웃!')
       localStorage.clear()
+      navigate('/login')
     })
     .catch((err) => {
       console.log('로그아웃 안된다 영원히..로그인..상태', err)
@@ -58,9 +72,9 @@ export default function MypageNavbar() {
           <img src={Me} alt="" style={{ height:80,width:80}}/>
         </div>
         <div className='p-7 pl-3'>
-          <h1 className='font-bold'>오승현</h1>
-          <h4 className='text-sm'>대구광역시 달서구 호산로 126</h4>
-          <h4 className='text-sm'style={{color:'gray'}}>상태메시지 입니다</h4>
+        <h1 className='font-bold'>{userInfo?.nickname}</h1>
+        <h4 className='text-sm'>{userInfo?.addressLegal}</h4>
+        <h4 className='text-sm'style={{color:'gray'}}>{userInfo?.comment}</h4>
         </div>
         {/* 아래의 디브는 본인의 마이페이지일경우만 보이고 , 즐찾이미지는 구매자일경우만 보임 */}
         <div className='flex justify-center items-center'>
