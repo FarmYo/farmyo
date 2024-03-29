b0d237d93573b8ef4eb03d94eea20d6bb1210eed
김준엽 <1025jy@naver.com>
2024-03-29 22:25:33 +0900

    🔨: createFarm 수정

--- backend/farmyo/src/main/java/com/ssafy/farmyo/myfarm/service/MyfarmService.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/myfarm/service/MyfarmService.java
@@ -2,12 +2,14 @@ package com.ssafy.farmyo.myfarm.service;
 
 import com.ssafy.farmyo.myfarm.dto.MyfarmDto;
 import com.ssafy.farmyo.myfarm.dto.MyfarmListDto;
-import com.ssafy.farmyo.myfarm.dto.MyfarmReqDto;
 import com.ssafy.farmyo.myfarm.dto.UpUserDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface MyfarmService {
 
-    void createFarm(MyfarmReqDto myfarmReqDto);
+    void createFarm(String loginId, String content, List<MultipartFile> files, List<Integer> orders);
     void updateFarm();
     void deleteFarm(int id);
     UpUserDto getUpUser(String loginId);
