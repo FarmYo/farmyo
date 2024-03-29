891a3060d24b03c523905a113d9d99fa4d7b7c8c
장세정 <wkdtpwjd19@gmail.com>
2024-03-29 19:35:26 +0900

    🔨 : 하단 네브바내리기

--- frontend/farmyo/src/App.jsx
+++ frontend/farmyo/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
   return (
     <div >
         <Router />
-        {!shouldHideBottomBar() && <BottomBar />}
+        {!shouldHideBottomBar() && <BottomBar className="bottomBar" />}
     </div>
   );
 }
