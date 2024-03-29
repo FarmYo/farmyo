eb3fdcd677f8aee09d217eea7a705f46ad156a87
김준엽 <wnsduq1025@gmail.com>
2024-03-29 22:06:32 +0900

    ✨: createFarm 기능 추가

--- backend/farmyo/src/main/java/com/ssafy/farmyo/myfarm/controller/MyfarmController.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/myfarm/controller/MyfarmController.java
@@ -1,6 +1,8 @@
 package com.ssafy.farmyo.myfarm.controller;
 
 import com.ssafy.farmyo.common.response.BaseResponseBody;
+import com.ssafy.farmyo.myfarm.dto.MyfarmReqDto;
+import com.ssafy.farmyo.myfarm.dto.MyfarmDto;
 import com.ssafy.farmyo.myfarm.dto.UpUserDto;
 import com.ssafy.farmyo.myfarm.service.MyfarmService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,8 +11,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -21,40 +27,53 @@ public class MyfarmController {
 
     private final MyfarmService myfarmService;
 
-    @PostMapping("")
+    @PostMapping(value = "", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     @Operation(summary = "마이팜 생성", description = "판매자는 userId와 사진, 게시글을 통해 마이팜 게시글을 생성한다.")
-    public ResponseEntity<? extends BaseResponseBody> createFarm() {
+    public ResponseEntity<? extends BaseResponseBody> createFarm(
+            @RequestParam("loginId") String loginId,
+            @RequestParam("content") String content,
+            @RequestPart("images") List<MultipartFile> files) {
+
+//        myfarmService.createFarm(myFarmReqDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponseBody.of(0, 0));
     }
 
     @GetMapping("/list")
     @Operation(summary = "마이팜 게시글 리스트 조회", description = "userId를 통해 작성한 마이팜 게시글 목록을 조회한다.")
-    public ResponseEntity<? extends BaseResponseBody> getFarms() {
+    public ResponseEntity<? extends BaseResponseBody> getFarms(
+            @RequestParam(name = "loginId")
+            @Parameter(description = "유저 아이디")
+            String loginId) {
 
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, 0));
     }
 
-    @GetMapping("/detail/{id}")
+    @GetMapping("")
     @Operation(summary = "마이팜 게시글 상세 조회", description = "마이팜 id를 통해 해당 마이팜 게시글을 상세 조회한다.")
     public ResponseEntity<? extends BaseResponseBody> getFarm(
-            @PathVariable(name = "id")
+            @RequestParam(name = "id")
             @Parameter(description = "마이팜 게시글 id")
             int id) {
 
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, 0));
     }
 
-    @PatchMapping("/{id}")
+    @PatchMapping("")
     @Operation(summary = "마이팜 게시글 수정", description = "마이팜 id와 수정할 정보를 통해 마이팜 게시글을 수정한다.")
-    public ResponseEntity<? extends BaseResponseBody> updateFarm() {
+    public ResponseEntity<? extends BaseResponseBody> updateFarm(
+            @RequestBody
+            MyfarmDto myfarmDto) {
 
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, 0));
     }
 
-    @DeleteMapping("/delete")
+    @DeleteMapping("")
     @Operation(summary = "마이팜 게시글 삭제", description = "마이팜 id를 통해 해당 마이팜 게시글을 및 사진을 삭제한다.")
-    public ResponseEntity<? extends BaseResponseBody> deleteFarm() {
+    public ResponseEntity<? extends BaseResponseBody> deleteFarm(
+            @RequestParam(name = "id")
+            @Parameter(description = "마이팜 게시글 삭제를 위한 마이팜 id")
+            int id) {
 
         return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, 0));
     }
