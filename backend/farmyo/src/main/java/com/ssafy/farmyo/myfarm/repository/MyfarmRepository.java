886d038979c695236b7e4813bf4a7520ca8cc6cb
김준엽 <wnsduq1025@gmail.com>
2024-03-30 23:23:43 +0900

    ✨: updateFarm, deleteFarm 기능 추가

--- backend/farmyo/src/main/java/com/ssafy/farmyo/myfarm/repository/MyfarmRepository.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/myfarm/repository/MyfarmRepository.java
@@ -2,8 +2,10 @@ package com.ssafy.farmyo.myfarm.repository;
 
 import com.ssafy.farmyo.entity.Farm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,5 +14,10 @@ public interface MyfarmRepository extends JpaRepository<Farm, Integer> {
 
     @Query(value = "SELECT f FROM Farm f WHERE f.farmer.id = :id ")
     List<Farm> findAllByUserId(int id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Farm f SET f.content = :content WHERE f.id = :id")
+    void updateFarm(int id, String content);
 
 }
