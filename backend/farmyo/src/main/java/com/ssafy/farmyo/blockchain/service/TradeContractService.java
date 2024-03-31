6d58a6ac5323d373a418dc8e358fd2949bdcc789
조현제 <abb6575@naver.com>
2024-03-31 18:47:31 +0900

    🚧 : 작업 도중 저장

---
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/blockchain/service/TradeContractService.java
@@ -0,0 +1,105 @@
+package com.ssafy.farmyo.blockchain.service;
+
+import com.ssafy.farmyo.blockchain.contract.CropContract;
+import com.ssafy.farmyo.blockchain.contract.TradeContract;
+import com.ssafy.farmyo.common.exception.CustomException;
+import com.ssafy.farmyo.common.exception.ExceptionType;
+import org.springframework.stereotype.Service;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.EthGasPrice;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.RawTransactionManager;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.StaticGasProvider;
+import org.web3j.tx.response.PollingTransactionReceiptProcessor;
+import org.web3j.tx.response.TransactionReceiptProcessor;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.List;
+
+
+@Service
+public class TradeContractService {
+
+    private TradeContract tradeContract;
+    private Web3j web3j;
+
+    private Credentials credentials;
+
+    private TransactionManager transactionManager;
+
+    public TradeContractService() throws IOException {
+
+        String rpcUrl = "https://rpc2.sepolia.org";
+        String privateKey = "59f616cf0e5003164c921f8172c9da1e7b5c1e1556eca22c0f0c9ef94a2a5b42";
+        String contractAddress = "0xE8448EEB2629E3e96f96f8EBedc9Fd2faa6fe20c";
+        long chainId = 11155111;
+
+
+        this.web3j = Web3j.build(new HttpService(rpcUrl)); // url과 web3j을 통해 해당 코인네트워크 접속
+        this.credentials = Credentials.create(privateKey);// 개인 키
+
+        EthGasPrice ethGasPrice = web3j.ethGasPrice().send();
+        BigInteger gasPrice = ethGasPrice.getGasPrice();
+
+        TransactionReceiptProcessor receiptProcessor = new PollingTransactionReceiptProcessor(
+                web3j,
+                TransactionManager.DEFAULT_POLLING_FREQUENCY,
+                TransactionManager.DEFAULT_POLLING_ATTEMPTS_PER_TX_HASH
+        );
+
+        this.transactionManager = new RawTransactionManager(
+                web3j,
+                credentials,
+                chainId,
+                receiptProcessor
+        );
+
+        // CropData 스마트 계약 로드, ContractGasProvider를 사용하여 가스 가격 및 한도 설정
+        this.tradeContract = TradeContract.load(
+                contractAddress,
+                web3j,
+                transactionManager,
+                new ContractGasProvider() {
+                    @Override
+                    public BigInteger getGasPrice(String contractFunc) {
+                        return gasPrice; // 동적으로 조회된 가스 가격 사용
+                    }
+
+                    @Override
+                    public BigInteger getGasPrice() {
+                        return gasPrice; // 동적으로 조회된 가스 가격 사용
+                    }
+
+                    @Override
+                    public BigInteger getGasLimit(String contractFunc) {
+                        return BigInteger.valueOf(3000000); // 예제 가스 한도
+                    }
+
+                    @Override
+                    public BigInteger getGasLimit() {
+                        return BigInteger.valueOf(3000000); // 예제 가스 한도
+                    }
+                }
+        );
+    }
+
+
+    public TransactionReceipt adminBurn(String address, BigInteger amount, BigInteger userId) throws Exception {
+        return tradeContract.adminBurn(address, amount, userId).send();
+    }
+
+    public TransactionReceipt adminMint(String address, BigInteger amount, BigInteger userId) throws Exception {
+        return tradeContract.adminMint(address, amount, userId).send();
+    }
+
+    public TransactionReceipt adminTransfer(String farmerAddress, String customerAddress, BigInteger amount, BigInteger customerId, BigInteger farmerId) throws Exception {
+        return tradeContract.adminTransfer(farmerAddress,customerAddress,amount,customerId,farmerId).send();
+    }
+
+}
+
