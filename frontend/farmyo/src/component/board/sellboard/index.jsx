8e51c39309a11e4ce5f6fadbac745263ca19f29a
권현준 <bonheur9813@naver.com>
2024-04-02 02:00:53 +0900

    🔨 : 게시판 생성 작물 카테고리 추가
    - 게시판 생성시 작물 카테고리 API 불러오기 및 드롭다운 구현
    - 선택시 렌더링 및 id값 상태변환

--- frontend/farmyo/src/component/board/sellboard/index.jsx
+++ frontend/farmyo/src/component/board/sellboard/index.jsx
@@ -226,7 +226,7 @@ export default function SellBoardList(){
     if (haveMore) {
       BoardInfo();
     }
-    setCropId(30);
+    setCropId(selectedCrop.id);
     // 크롭 id 바꾸기
   }, [])
 
@@ -301,7 +301,7 @@ export default function SellBoardList(){
             <Menu as="div" className="relative inline-block text-left">
             <div>
               <Menu.Button className="inline-flex w-44 justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                작물명을 선택하세요
+                {selectedCrop.categoryName}
                 <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
               </Menu.Button>
             </div>
@@ -314,7 +314,7 @@ export default function SellBoardList(){
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-            <Menu.Items className="absolute right-0 z-10 w-full px-4 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            <Menu.Items className="absolute right-0 z-10 w-full px-4 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none overflow-y-auto max-h-60"
           style={{width:'16rem'}}>
             <div className="py-1">
               {cropList.map((crop,index)=>(
