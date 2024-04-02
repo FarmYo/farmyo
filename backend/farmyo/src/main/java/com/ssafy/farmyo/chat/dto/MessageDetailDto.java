d3fc8c5ef6d11b4a4af3f6c542c7922f2b2bf386
timber3 <timber3@naver.com>
2024-04-03 04:16:01 +0900

    🔨 : pagination 코드 로직 변경

--- backend/farmyo/src/main/java/com/ssafy/farmyo/chat/dto/MessageDetailDto.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/chat/dto/MessageDetailDto.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 public class MessageDetailDto {
+    private int messageId; // msg ID 값 ( 이전 기록을 불러오기 위한 기준이 되는 값 )
     private int userId; // 누가 보냈는지
     private String content;  // 무슨 내용을
     private LocalDateTime createdAt;  // 언제
