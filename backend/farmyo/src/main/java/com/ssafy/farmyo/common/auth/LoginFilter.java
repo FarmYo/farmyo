611851bfc93cd35ea415708e3f33ab261edc7c04
권현준 <bonheur9813@naver.com>
2024-04-02 11:00:04 +0900

    ✨ : 재발급 요청 인터셉터 구현
    - 인터셉터를 활용한 만료 응답 토큰 재발급 요청 처리

--- backend/farmyo/src/main/java/com/ssafy/farmyo/common/auth/LoginFilter.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/common/auth/LoginFilter.java
@@ -103,8 +103,11 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         log.info("Create Token - loginId : {}, id : {}, job : {}, nickname : {}", username, id, job, nickname);
 
         //토큰 생성
-        String access = jwtUtil.createJwt("access", username, nickname, id,  job, 86400000L);
+//        String access = jwtUtil.createJwt("access", username, nickname, id,  job, 86400000L);
+        String access = jwtUtil.createJwt("access", username, nickname, id,  job, 600000L);
         String refresh = jwtUtil.createJwt("refresh", username, nickname, id, job,  86400000L);
+
+
 
         // Refresh 토큰 Redis 저장
         refreshTokenRepository.save(new RefreshToken(refresh, username, access));
