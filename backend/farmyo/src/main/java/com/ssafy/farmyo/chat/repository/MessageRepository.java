3def1f7777b4533ef5065196f95f6e9a3d6054e4
timber3 <timber3@naver.com>
2024-04-03 01:54:13 +0900

    ✨ : 안 읽은 메세지 개수 확인하기

--- backend/farmyo/src/main/java/com/ssafy/farmyo/chat/repository/MessageRepository.java
+++ backend/farmyo/src/main/java/com/ssafy/farmyo/chat/repository/MessageRepository.java
@@ -6,6 +6,7 @@ import com.ssafy.farmyo.entity.Message;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -24,4 +25,17 @@ public interface MessageRepository extends JpaRepository<Message, Integer> {
     @Query("SELECT new com.ssafy.farmyo.chat.dto.MessageDetailDto(m.userId, m.content, m.createdAt) from Message m WHERE m.chat.id=:chatId ORDER BY m.createdAt DESC")
     List<MessageDetailDto> findAllById(int chatId, Pageable pageable);
 
+    @Query("select count(*) from Message m where m.chat.id=:chatId and m.sellerRead=0")
+    Integer getSellerUnreadMessageCount(int chatId);
+
+    @Query("select count(*) from Message m where m.chat.id=:chatId and m.buyerRead=0")
+    Integer getBuyerUnreadMessageCount(int chatId);
+
+    @Modifying
+    @Query("update Message m set m.buyerRead=1 where m.chat.id=:chatId")
+    void readBuyerMessages(int chatId);
+
+    @Modifying
+    @Query("update Message m set m.sellerRead=1 where m.chat.id=:chatId")
+    void readSellerMessages(int chatId);
 }
