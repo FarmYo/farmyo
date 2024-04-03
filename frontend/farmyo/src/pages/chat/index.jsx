6f91308680a90ccc36cbf89e333e2407f22a58ac
장세정 <wkdtpwjd19@gmail.com>
2024-04-03 21:43:53 +0900

    ✨ : 채팅 안읽은수 표시

--- frontend/farmyo/src/pages/chat/index.jsx
+++ frontend/farmyo/src/pages/chat/index.jsx
@@ -9,11 +9,13 @@ export default function Chatting(){
   // const isAuthed = useTokenCheck();
   const [chattingData, setChattingData] = useState([])
   const loginId = jwtDecode(localStorage.getItem('access')).loginId
+
   const showChattings = (() => {
     api.get(`chats/rooms/${loginId}`)
     .then((res) => {
       console.log('채팅방 목록 불러오기 성공', res)
       setChattingData(res.data.dataBody.resultList)
+
     })
     .catch((err) => {
       console.log('채팅방 목록 불러오기 실패', err)
@@ -31,15 +33,29 @@ useEffect(() => {
       </div>
       {/* 채팅목록나옴 */}
       {chattingData.map((chat) => (
-        <div onClick={() => {navigate(`/chat/${chat.chatId}`)}} key={chat.chatId} className='p-3 flex border-b-2 border-gray-100'>
-          <div>
-            {/* 채팅 상대방 프로필 사진 나옴 */}
-            <img src={chat.userProfile} alt="" style={{width: 50}}/>
+        <div onClick={() => {navigate(`/chat/${chat.chatId}`)}} key={chat.chatId} className='p-2 flex justify-between border-b-2 border-gray-100'
+        style={{height: '80px'}}>
+          <div className='flex'>
+            <div>
+              <img src={chat.userProfile} alt="" style={{width: 60,height:60}}/>
+            </div>
+            <div className='ml-3'>
+              <h1 className='font-bold'>{chat.userNickname}</h1>
+              <h1 className='text-sm'>{chat.recentMessage}</h1>
+            </div>
           </div>
-          <div className='ml-3'>
-            <h1 className='font-bold'>{chat.userNickname}</h1>
-            <h1 className='text-sm'>{chat.recentMessage}</h1>
-          </div> 
+      
+          <div className="flex flex-col justify-between h-full">
+            <div className="flex justify-center items-center" style={{height: '24px', minWidth: '24px', width: '24px'}}>
+              {chat.unreadCount > 0 && (
+                <div className="flex items-center justify-center" style={{backgroundColor: 'red', borderRadius: '50%', color: 'white', width: '100%', height: '100%'}}>
+                  <div>{chat.unreadCount}</div>
+                </div>
+              )}
+            </div>
+            <div className="text-xs text-center mt-auto">{chat.boardTitle}</div>
+          </div>
+
         </div>
       ))}
     </div>
