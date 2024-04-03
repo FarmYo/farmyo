ce7f781136399ed94c11c8d68547a46168fb341a
권현준 <bonheur9813@naver.com>
2024-04-03 11:02:47 +0900

    🔨 : 팜& 삼 헤더 컴포넌트 분리
    팜 & 삼 헤더 컴포넌트 분리

--- frontend/farmyo/tailwind.config.js
+++ frontend/farmyo/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '50px': '50px', // 커스텀 스페이싱 추가
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
